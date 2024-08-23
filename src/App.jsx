fa2780588542577bfb13b4b11fcea95207184438
Mayurwagh98 <mswagh98@gmail.com>
2024-08-23 11:16:21 +0530

    Added: bg2 and bg3

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import { useState } from "react";
 import "./App.css";
 import MovingShapes from "./components/Bg1";
+import Bg2 from "./components/Bg2";
+import Bg3 from "./components/Bg3";
 
 function App() {
   return (
     <>
-      <MovingShapes />
+      {/* <MovingShapes /> */}
+      <Bg3 />
     </>
   );
 }
---
+++ src/components/Bg2.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import "./Bg2.css"
+
+const Bg2 = () => {
+  return (
+    <div class="wrapper">
+      <div class="box">
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
+    </div>
+  );
+};
+
+export default Bg2;
